83b225f8d081b915125ae28d1e7cb98f07f2212a
xiaoyu1998 <1634054493@qq.com>
2020-04-02 15:31:12 +0800

    examples:update msig with regid

--- examples/msig/include/msig.hpp
+++ examples/msig/include/msig.hpp
@@ -22,13 +22,13 @@ CONTRACT msig : public contract {
       ACTION file1(uint64_t p1, uint64_t p2);
       ACTION file2(string   p);
       
-      ACTION propose(name proposer, name proposal_name, file_t file);
-      ACTION approve(name proposer, name proposal_name, name approver);
-      ACTION exec( name proposer, name proposal_name, name executer );
+      ACTION propose(regid proposer, name proposal_name, file_t file);
+      ACTION approve(regid proposer, name proposal_name, regid approver);
+      ACTION exec( regid proposer, name proposal_name, regid executer );
       // ACTION unapprove(name proposer, name proposal_name, name unapprover);
 
       ACTION setthreshold(name file, uint64_t threshold);
-      ACTION setadmin(name admin,    uint64_t weight);
+      ACTION setadmin(regid admin,    uint64_t weight);
   private:
       TABLE proposal_t {
           name   proposal_name;
@@ -58,13 +58,13 @@ CONTRACT msig : public contract {
       };
 
       TABLE admin_t {
-          name     account;
+          regid     account;
           uint64_t weight;
-          name  primary_key()const { return account; }
+          regid  primary_key()const { return account; }
       };
 
       typedef wasm::table< "proposals"_n, proposal_t, name > proposals;  
       typedef wasm::table< "approvals2"_n, approvals_info_t, name > approvals2; 
       typedef wasm::table< "authorities"_n, authority_t, name > authorities; 
-      typedef wasm::table< "admins"_n, admin_t, name > admins; 
+      typedef wasm::table< "admins"_n, admin_t, regid > admins; 
 };
