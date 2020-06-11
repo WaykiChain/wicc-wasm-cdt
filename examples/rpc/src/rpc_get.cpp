9480bfce6ed763080840d93c23ec974c7be26716
chenxinhua2018 <15220088021@163.com>
2020-06-11 16:17:27 +0800

    add set_result()

--- examples/rpc/src/rpc_get.cpp
+++ examples/rpc/src/rpc_get.cpp
@@ -55,8 +55,7 @@ ACTION rpc::get_account(regid id, symbol_code sym){
     check_fmt(accts.get(acct, sym.raw()), "the account not exist! id=%s, sym=%s",
           id.to_string().c_str(), sym.to_string().c_str());
 
-    auto value = wasm::pack<account>(acct);
-    set_rpc_result("account", "account", value);
+    set_result("account", "account", acct);
 }
 
 // void rpc::sub_balance( regid owner, asset value ) {
--- libraries/wasmlib/rpc/rpc.hpp
+++ libraries/wasmlib/rpc/rpc.hpp
@@ -10,4 +10,16 @@ namespace wasm {
  */
 void set_rpc_result(const string &name, const string &type, const std::vector<char> &value);
 
+
+/**
+ *  set the result for wasm rpc.
+ *
+ *  @ingroup set_result
+ */
+  template <typename T>
+  inline void set_result(const string &name, const string &type, const T &value) {
+    auto v = wasm::pack<T>(value);
+    set_rpc_result(name, type, v);
+  }
+
 } // namespace wasm
