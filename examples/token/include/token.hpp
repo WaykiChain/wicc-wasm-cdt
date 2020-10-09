e9316b1c91bbf01c672736efa66f9e22a404d6ae
hpy <hpy@wiccdeMac-mini.local>
2020-10-09 17:56:58 +0800

    delete get_supply and get_balance interface

--- examples/token/include/token.hpp
+++ examples/token/include/token.hpp
@@ -22,22 +22,6 @@ namespace wasm {
          ACTION transfer(regid from, regid to, asset quantity, string  memo );
          ACTION open (regid owner, const symbol& symbol, regid payer );
          ACTION close(regid owner, const symbol& symbol );
-
-         static asset get_supply( regid token_contract_account, symbol_code sym_code )
-         {  
-            currency_stats st;
-            st.supply.symbol.code() = sym_code;
-            db::get(st);
-            return st.supply;
-         }
-
-         static asset get_balance( regid token_contract_account, regid owner, symbol_code sym_code )
-         {
-            account ac;
-            ac.balance.symbol.code() = sym_code;
-            db::get( ac );
-            return ac.balance;
-         }
          
       private:
          void sub_balance(regid owner, asset value);
